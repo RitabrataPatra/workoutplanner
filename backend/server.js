3308e940268c548bbb073459e0e6bc7a9f556aa2
Ritabrata Patra <teamritabrata@gmail.com>
2025-01-07 22:23:18 +0530

    connection initiated to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config( { path: "./.env" } );
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+const connectionURI = process.env.MONGODB_URI
 
+mongoose.connect(connectionURI).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
 app.listen(port , ()=>{
     console.log(`Server is running on port ${port}`);
 })
