8857a1581d18daf85d8f99023cfe8e2697a1426c
Ritabrata Patra <teamritabrata@gmail.com>
2025-01-07 21:19:00 +0530

    installed material ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import SignUpPage from './pages/SignUpPage'
 import {Routes, Route} from "react-router-dom"
 function App() {
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,20 @@
-
+import Button from '@mui/material/Button';
 const SignUpPage = () => {
   return (
-    <div>
-        <h1 className="text-5xl text-red-500">Hello </h1>
-    </div>
-  )
-}
+    <main>
+      <div className="border border-red-700">
+        <h1 className="text-5xl text-red-500">Welcome to BeingFitter💪</h1>
+      </div>
+      <div className="border border-blue-700 mt-4 py-4">
+        <form action="" className="flex flex-col gap-4 items-center border border-black justify-center">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <Button variant="contained" type='submit'>Sign Up</Button>;
+        </form>
+      </div>
+    </main>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
