4b9f085c70939b8db68814934f926829accd95ab
Ritabrata Patra <teamritabrata@gmail.com>
2025-01-07 22:04:10 +0530

    login and signup page frontend created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
 
 import './App.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import {Routes, Route} from "react-router-dom"
 function App() {
 
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
       </Routes>
-    </div>
+    </>
   )
 }
 
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,32 +2,25 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
-const SignUpPage = () => {
+const LoginPage = () => {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
       {/* Header Section */}
       <div className="text-center mb-8">
         <h1 className="text-5xl font-bold text-red-500 mb-2">
-          Welcome to BeingFitter 💪
+          Welcome Back to BeingFitter 💪
         </h1>
         <p className="text-gray-600 text-lg">
-          Your fitness journey starts here. Sign up and get moving!
+          Just login and get moving!
         </p>
       </div>
 
       {/* Form Section */}
       <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
         <h2 className="text-3xl font-semibold text-gray-800 mb-6 text-center">
-          Sign Up
+          Login
         </h2>
         <form className="flex flex-col gap-6">
-          <TextField
-            id="name"
-            label="Name"
-            variant="outlined"
-            fullWidth
-            className="bg-gray-50"
-          />
           <TextField
             id="email"
             label="Email"
@@ -49,13 +42,13 @@ const SignUpPage = () => {
             fullWidth
             className=" text-white py-2 font-semibold"
           >
-            Sign Up
+           Login
           </Button>
         </form>
         <p className="text-sm text-gray-600 mt-4 text-center">
-          Already have an account?{" "}
-          <a href="/login" className="text-red-500 hover:underline">
-            Log in
+          Don&apos;t have an account?{" "}
+          <a href="/signup" className="text-red-500 hover:underline">
+            Create account
           </a>
         </p>
       </div>
@@ -63,4 +56,4 @@ const SignUpPage = () => {
   );
 };
 
-export default SignUpPage;
+export default LoginPage;
