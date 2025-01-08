cdf6b52cd7b78c096015bd6ee2248570b5549bd3
Ritabrata Patra <teamritabrata@gmail.com>
2025-01-08 22:56:49 +0530

    now able to fetch data on dashboad

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,48 @@
-
+import { useEffect, useState } from "react"
+import axios from "axios";
 
 const Dashboard = () => {
+  const[userData , setUserData] = useState([]);
+  const[isLoading , setIsLoading] = useState(false);
+  const getUserList = async () => {
+    setIsLoading(true);
+    try {
+      const {data} = await axios.get("http://localhost:8000/api/users");
+      console.log(data);
+      if(data){
+        setUserData(data.users)
+      }
+      
+    } catch (error) {
+      alert("Error in getting users");
+      console.log(error);
+    }finally{
+      setIsLoading(false);
+    }
+  }
+ 
+  useEffect(() => {
+    getUserList();
+  } ,[])
+
+  
   return (
-    <div>Welcome to Your Dashboard</div>
+    <>
+    {
+      isLoading ? <div>Loading...</div> : (
+        <div>
+          <h1>Dashboard , total users : {userData.length}</h1>
+          { userData.length > 0 &&
+            userData.map((user) => (
+              <div key={user._id}>
+                <h1>{user.name}</h1>
+              </div>
+            ))
+          }
+        </div>
+      ) 
+    }
+    </>
   )
 }
 
